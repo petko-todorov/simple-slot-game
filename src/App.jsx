774bfdc352f76ec0766e74c7832769649b8c7c6b
Petko Todorov <p.todorov1015@gmail.com>
2025-06-05 14:38:13 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import SlotMachine from './components/SlotMachine'
+
+function App() {
+
+    return (
+        <>
+            <h1 className='text-6xl font-bold text-center text-amber-400'>
+                Test
+            </h1>
+
+            <SlotMachine />
+        </>
+    )
+}
+
+export default App
