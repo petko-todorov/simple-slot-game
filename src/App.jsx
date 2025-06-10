6559a3eb8c89af79761e24c53e61bd0985a14572
Petko Todorov <p.todorov1015@gmail.com>
2025-06-10 15:08:27 +0300

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,17 @@
-// import { useState } from 'react'
-// import SlotMachine from './components/SlotMachine'
-// import SpinButton from './components/SpinButton'
-
-// function App() {
-
-//     return (
-//         <>
-//             <div className="h-screen flex flex-col items-center justify-center">
-//                 <h1 className='text-6xl font-bold text-center text-amber-400'>
-//                     Test
-//                 </h1>
-
-//                 <SlotMachine />
-
-//             </div>
-//         </>
-//     )
-// }
-
-// export default App;
-
-
 import { useState } from 'react'
 import SlotMachine from './components/SlotMachine'
 import SpinButton from './components/SpinButton'
+import BalanceDisplay from './components/BalanceDisplay';
+import BetControls from './components/BetControls';
+import PayoutTable from './components/PayoutTable';
+
+const balance = 5000;
 
 function App() {
     const [isSpinning, setIsSpinning] = useState(false);
     const [autoAnimationStart, setAutoAnimationStart] = useState(false);
     const [triggerSpin, setTriggerSpin] = useState(0);
+    const [bet, setBet] = useState(500);
 
     const handleSpin = () => {
         if (isSpinning) return;
@@ -45,7 +28,7 @@ function App() {
 
     return (
         <>
-            <div className="h-screen flex flex-col items-center justify-center">
+            <div className="h-screen flex flex-col items-center justify-center gap-5 w-1/2 mx-auto">
                 <h1 className='text-6xl font-bold text-center text-amber-400'>
                     Test
                 </h1>
@@ -56,9 +39,20 @@ function App() {
                     triggerSpin={triggerSpin}
                 />
 
+                <PayoutTable />
+
+                <BetControls
+                    bet={bet}
+                    setBet={setBet}
+                />
+
                 <SpinButton
                     handleSpin={handleSpin}
                     isSpinning={isSpinning}
+                />
+
+                <BalanceDisplay
+                    balance={balance}
                 />
             </div>
         </>
