574df69e1196157065daa76b7cf696557254f970
Petko Todorov <p.todorov1015@gmail.com>
2025-06-12 21:15:59 +0300

    Add return

--- src/components/SpinButton.jsx
+++ src/components/SpinButton.jsx
@@ -1,11 +1,15 @@
-const SpinButton = ({ handleSpin, isSpinning }) => (
-    <button
-        onClick={handleSpin}
-        className={`${isSpinning ? "cursor-not-allowed opacity-50" : "cursor-pointer"} bg-amber-600 text-black font-bold text-lg w-1/3 py-5 rounded-lg`}
-        disabled={isSpinning}
-    >
-        {isSpinning ? 'Spinning...' : 'Spin'}
-    </button>
-);
+const SpinButton = ({ handleSpin, isSpinning }) => {
+    return (
+
+        <button
+            onClick={handleSpin}
+            className={`${isSpinning ? "cursor-not-allowed opacity-50" : "cursor-pointer"} bg-amber-600 text-black font-bold text-lg w-1/3 py-5 rounded-lg`}
+            disabled={isSpinning}
+        >
+            {isSpinning ? 'Spinning...' : 'Spin'}
+        </button>
+    );
+}
+
 
 export default SpinButton;
